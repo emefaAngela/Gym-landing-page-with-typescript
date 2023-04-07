28dfa160d3cd9d16cc857e3691d1ca484243063c
emefaAngela <gelamengor@gmail.com>
2023-04-07 23:53:39 +0000

    designing the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import Navbar from "@/scenes/navbar";
+import Navbar from "@/scenes/navbar/navbar";
+import Home from "./scenes/navbar/home";
 import { useEffect } from "react";
 import { useState } from "react";
 import { SelectedPage } from "./shared/types";
 
-
 function App() {
- const[selectedPage,setSelectedPage]= useState<SelectedPage>(SelectedPage.Home); 
- const[isTopOfPage,setIsTopOfPage]= useState<boolean>(true);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
- useEffect(()=>{
- const handleScroll=()=>{
-  if(window.scrollY===0){
-    setIsTopOfPage(true);
-    setSelectedPage(SelectedPage.Home);
-  }
-  if(window.scrollY!==0)setIsTopOfPage(false);
- }
- window.addEventListener("scroll",handleScroll);
- return ()=>window.removeEventListener("scroll",handleScroll);
- }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20  ">
       <Navbar
-      isTopOfPage={isTopOfPage}
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
